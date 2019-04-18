b6c720f79dbcded3d8a59c54f8f5ea165320c9e4
FABIO LUIS GABRIEL VIEIRA <fabio.lgvieira@senacsp.edu.br>
2019-04-18 08:59:07 -0300

    Alteração Fabio

--- src/prjjava1/PrjJava1.java
+++ src/prjjava1/PrjJava1.java
@@ -17,6 +17,7 @@ public class PrjJava1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println(" Hello World");
+        System.out.println("Alterado para novo commit");
     }
     
 }
